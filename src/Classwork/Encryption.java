7ce5a4164f9e7350535711f86b640497ecb96492
pthapagit <prabesh.usa@gmail.com>
2021-03-09 22:59:49 -0600

    added src folder

---
+++ src/Classwork/Encryption.java
@@ -0,0 +1,8 @@
+package Classwork;
+
+public class Encryption {
+    public static void main(String[] args) {
+        System.out.println("Prabesh Thapa");
+        
+    }
+}
---
+++ src/branch1/Branch1.java
@@ -0,0 +1,8 @@
+package branch1;
+
+public class Branch1 {
+    public static void main(String[] args) {
+        System.out.println("Branch1");
+
+    }
+}
